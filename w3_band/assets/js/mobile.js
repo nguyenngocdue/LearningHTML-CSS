5d746f6a52b1f6d6091d82dd11cc1f01137284ec
NguyenNgocDue <duengocnguyen@gmail.com>
2021-10-28 11:13:30 +0700

    Moblie menu fix bug

---
+++ w3_band/assets/js/mobile.js
@@ -0,0 +1,24 @@
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight;
+
+// Đóng / mở mobile menu
+mobileMenu.onclick = function () {
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose) {
+        header.style.height = 'auto';
+    }
+    else {
+        header.style.height = null;
+    }
+}
+
+//Tự động đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    menuItem.onclick = function () {
+        header.style.height = null;
+    }
+}
